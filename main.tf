2ad294b9b94189a6ac08acd99582342639b9430c
SREEKANTH A <110668483+SREEKANTH1313@users.noreply.github.com>
2023-01-30 15:14:23 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@ provider "aws"{
 }
 
 resource "aws_instance" "server" {
-  ami           = "ami-095413544ce52437d"
+  ami           = "ami-06e85d4c3149db26a"
   instance_type = "t2.micro"
   subnet_id="subnet-0e12930bf6bfb0b0d"
   security_groups=["sg-086b130f73f065639"]
